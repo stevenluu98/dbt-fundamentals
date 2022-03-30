02d523be68c9d001133ab65c7e89970b9e2cf82e
stevenluu98 <stevenluu98@gmail.com>
2022-03-30 18:21:37 -0400

    modified dim_customers to support lifetime_value field

--- jaffle_shop/models/marts/core/dim_customers.sql
+++ jaffle_shop/models/marts/core/dim_customers.sql
@@ -3,7 +3,7 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
@@ -13,14 +13,14 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders.
+        sum(amount) as lifetime_value
 
     from orders
 
     group by 1
 
 ),
-
 
 final as (
 
@@ -30,7 +30,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
 
     from customers
 
